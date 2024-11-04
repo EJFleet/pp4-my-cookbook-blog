48767502a8e2065858cf672c21f9c8b1468e1916
Emma-Jane Fleetwood <emmafleetwood@gmail.com>
2024-11-04 20:11:21 +0000

    Set up secret key

--- my_cookbook/settings.py
+++ my_cookbook/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k$@urwu_!&)o0h50z$+$17#lv7c6%ela3#yh)@k^6m3u5mjspa'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
