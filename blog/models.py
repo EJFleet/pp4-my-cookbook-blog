2edf77172fbf92337c34a0c06566d1570c83d9a7
Emma-Jane Fleetwood <emmafleetwood@gmail.com>
2024-11-05 12:16:10 +0000

    Add Recipe model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+class Recipe(models.Model):
+    """
+    A model to create and manage recipes
+    """
+    title = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_writer")
+    description = models.CharField(max_length=500, null=False, blank=False)
+    posted_date = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    class Meta:
+        ordering = ['-posted_date']
+
+    def __str__(self):
+        return str(self.title)
