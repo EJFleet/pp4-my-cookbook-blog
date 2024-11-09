f47d3d64169ce717fc20c7220efddcb3a24bcddc
Emma-Jane Fleetwood <emmafleetwood@gmail.com>
2024-11-09 16:58:46 +0000

    Add recipe_detail view with placeholder html from CI

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.RecipeList.as_view(), name='home')
+    path('', views.RecipeList.as_view(), name='home'),
+    path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
 ]
