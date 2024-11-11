cd2fac20c1d5bef9775b06a2a47bd866fa9440be
Emma-Jane Fleetwood <emmafleetwood@gmail.com>
2024-11-11 13:58:51 +0000

    Add edit and delete functionality for comments

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
     path('recipe/<slug:slug>/', views.recipe_detail, name='recipe_detail'),
+    path('recipe/<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name="comment_edit"),
+    path('recipe/<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
 ]
