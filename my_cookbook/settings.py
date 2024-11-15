3289e9921c049d649a0d47dd41920ebbf6e849ee
Emma-Jane Fleetwood <emmafleetwood@gmail.com>
2024-11-15 12:03:27 +0000

    Make minor changes for debugging Heroku

--- my_cookbook/settings.py
+++ my_cookbook/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -52,7 +53,6 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
-    'cloudinary_storage',
     'cloudinary',
     'blog',
 ]
