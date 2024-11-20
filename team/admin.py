3dd308d0e2e1c1ef35758066c485710a05a4f612
Emma-Jane Fleetwood <emmafleetwood@gmail.com>
2024-11-20 15:34:02 +0000

    Add bio field to TeamMember and add admin.py details

--- team/admin.py
+++ team/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TeamMember
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(TeamMember)
+class TeamMemberAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'jobtitle', 'location')
+    search_fields = ['name']
+    summernote_fields = ('bio',)
--- team/models.py
+++ team/models.py
@@ -7,13 +7,14 @@ class TeamMember(models.Model):
     """
     Create profiles for team members
     """
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, null=False, blank=False)
+    bio = models.TextField(max_length=1000, null=False, blank=False, default="A wonderul team member!")
     jobtitle = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
     headshot = CloudinaryField('image', default='placeholder')
 
     class Meta:
-        ordering = ["name"]
+        ordering = ['name']
 
     def __str__(self):
         return self.name
