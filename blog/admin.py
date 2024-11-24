fc8b2c9966f55a538358c3568a995c3d21d0cb19
Emma-Jane Fleetwood <emmafleetwood@gmail.com>
2024-11-24 14:35:44 +0000

    Change comment admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Recipe, Comment
 from .forms import RecipeAdminForm
+
+
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
     form = RecipeAdminForm
@@ -12,5 +14,10 @@ class RecipeAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('ingredients', 'method',)
 
-# Register your models here.
-admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'author', 'body', 'created_on')
+    list_filter = ('recipe', 'created_on') 
+    search_fields = ('body', 'author__username', 'recipe__title')
+    
