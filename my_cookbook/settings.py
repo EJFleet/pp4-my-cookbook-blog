ee722b27bf291f93a9c5caf4db0f65aeaaea89d8
Emma-Jane Fleetwood <emmafleetwood@gmail.com>
2024-11-29 20:42:36 +0000

    Set debug to False

--- my_cookbook/settings.py
+++ my_cookbook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-ejfleet-pp4mycookbookbl-ty21u2bpw8j.ws.codeinstitute-ide.net',
